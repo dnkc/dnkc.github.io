f5e2adcd5bb6ca31d62b3e8bbdb2cc1c27b60730
dnkc <dnkcdesign@gmail.com>
2022-03-31 23:23:49 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
 const withOptimizedImages = require("next-optimized-images");
 
 const nextConfig = withOptimizedImages({
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: isProd ? "https://dnkc.github.io/" : "",
   reactStrictMode: true,
   exportPathMap: async function (
     defaultPathMap,
