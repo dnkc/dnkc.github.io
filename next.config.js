50eb486258baf455d5de366d2c3a77e2e95b08ff
dnkc <dnkcdesign@gmail.com>
2022-03-31 23:19:19 -0400

    test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@
 const withOptimizedImages = require("next-optimized-images");
 
 const nextConfig = withOptimizedImages({
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   exportPathMap: async function (
     defaultPathMap,
